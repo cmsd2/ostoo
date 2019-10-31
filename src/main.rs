d34520d03cb85bd2fba0a46719cc6aaf83cafd7f
Chris Dawes <cmsd2@cantab.net>
2019-10-31 19:24:00 +0000

    unit tests and integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
+use os::println;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -20,12 +18,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
+    os::test_panic_handler(info)
 }
 
 #[no_mangle]
@@ -36,30 +29,4 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     panic!("Some panic message");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
 }
