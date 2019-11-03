c90084a8e09d60f93e77ae159e1fa3ec3e05a676
Chris Dawes <cmsd2@cantab.net>
2019-11-03 12:19:23 +0000

    try and read acpi tables

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,11 +6,20 @@
 
 extern crate alloc;
 
+use core::alloc::Layout;
 use core::panic::PanicInfo;
+use core::ptr::NonNull;
 use bootloader::{BootInfo, entry_point};
 use libkernel::{println, init, hlt_loop};
+use libkernel::memory::{VmemAllocator, DumbVmemAllocator};
+use x86_64::{PhysAddr, VirtAddr, align_up};
+use x86_64::structures::paging::{Page, PageSize, PageTableFlags, PhysFrame, FrameAllocator, Size4KiB, Mapper};
+use linked_list_allocator::LockedHeap;
+use acpi::handler::{PhysicalMapping, AcpiHandler};
 
 pub const APIC_BASE: u64 = 0x_5555_5555_0000;
+pub const ACPI_HEAP_BASE: u64 = 0x_6666_6666_0000;
+pub const ACPI_HEAP_SIZE: u64 = 100 * 4096;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -31,7 +40,6 @@ entry_point!(libkernel_main);
 pub fn libkernel_main(boot_info: &'static BootInfo) -> ! {
     use libkernel::memory;
     use libkernel::allocator;
-    use x86_64::VirtAddr;
     use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
 
     println!("Hello World{}", "!");
@@ -65,8 +73,68 @@ pub fn libkernel_main(boot_info: &'static BootInfo) -> ! {
     core::mem::drop(reference_counted);
     println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
+    unsafe {
+        // TODO: get acpi table addresses from bootloader
+        acpi::search_for_rsdp_bios(&mut AcpiMapper {
+            vmem_allocator: DumbVmemAllocator::new(VirtAddr::new(ACPI_HEAP_BASE), ACPI_HEAP_SIZE),
+            page_table: &mut mapper,
+            frame_allocator: &mut frame_allocator,
+        }).expect("acpi");
+    }
+
     #[cfg(test)]
     test_main();
 
     hlt_loop();
+}
+
+struct AcpiMapper<'a, F, M> where F: FrameAllocator<Size4KiB>, M: Mapper<Size4KiB> {
+    vmem_allocator: DumbVmemAllocator<Size4KiB>,
+    page_table: &'a mut M,
+    frame_allocator: &'a mut F,
+}
+
+impl <'a, F, M> AcpiHandler for AcpiMapper<'a, F, M> where F: FrameAllocator<Size4KiB>, M: Mapper<Size4KiB> {
+    fn map_physical_region<T>(
+        &mut self, 
+        physical_address: usize, 
+        size: usize
+    ) -> PhysicalMapping<T> {
+        let start_frame = PhysFrame::<Size4KiB>::containing_address(PhysAddr::new(physical_address as u64));
+        let end_frame = PhysFrame::<Size4KiB>::containing_address(PhysAddr::new(physical_address as u64 + size as u64).align_up(Size4KiB::SIZE));
+        let frame_range = PhysFrame::<Size4KiB>::range(start_frame, end_frame);
+
+        let layout_size = align_up(size as u64, Size4KiB::SIZE);
+        let (start_page, end_page) = self.vmem_allocator.alloc(layout_size / Size4KiB::SIZE);
+        let page_range = Page::<Size4KiB>::range_inclusive(start_page, end_page);
+
+        let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+        for (frame, page) in frame_range.zip(page_range) {
+            unsafe {
+                self.page_table.map_to(page, frame, flags, self.frame_allocator)
+                    .expect("map_to")
+                    .flush();
+            }
+        }
+
+        PhysicalMapping {
+            physical_start: start_frame.start_address().as_u64() as usize,
+            virtual_start: NonNull::new(start_page.start_address().as_mut_ptr()).unwrap(),
+            region_length: size,
+            mapped_length: layout_size as usize,
+        }
+    }
+
+    fn unmap_physical_region<T>(&mut self, region: PhysicalMapping<T>) {
+        let start_page = Page::from_start_address(VirtAddr::new(region.virtual_start.as_ptr() as usize as u64)).expect("page");
+        let end_page = start_page + region.mapped_length as u64 / Size4KiB::SIZE;
+        let page_range = Page::<Size4KiB>::range(start_page, end_page);
+
+        for page in page_range {
+            let (_frame, flush) = self.page_table.unmap(page).expect("unmap");
+            // could free frame except dumb vmem allocator does nothing
+            flush.flush();
+        }
+    }
 }
---
+++ libkernel/src/memory/vmem_allocator.rs
@@ -0,0 +1,52 @@
+use x86_64::VirtAddr;
+use x86_64::structures::paging::{Page, PageSize};
+
+pub trait VmemAllocator<S> where S: PageSize {
+    fn alloc(&mut self, num_pages: u64) -> (Page<S>, Page<S>);
+    fn dealloc(&mut self, start: Page<S>, end: Page<S>);
+}
+
+pub struct DumbVmemAllocator<S> where S: PageSize {
+    base: Page<S>,
+    next: Page<S>,
+    end: Page<S>,
+}
+
+impl <S> DumbVmemAllocator<S> where S: PageSize {
+    pub fn new(base_addr: VirtAddr, size_bytes: u64) -> Self {
+        let base = Page::from_start_address(base_addr).expect("page");
+        let end = Page::from_start_address((base_addr + size_bytes).align_up(S::SIZE)).expect("page");
+
+        DumbVmemAllocator {
+            base: base,
+            next: base,
+            end: end,
+        }
+    }
+
+    pub fn available(&self) -> u64 {
+        self.end - self.next
+    }
+
+    pub fn used(&self) -> u64 {
+        self.next - self.base
+    }
+}
+
+impl <S> VmemAllocator<S> for DumbVmemAllocator<S> where S: PageSize {
+    fn alloc(&mut self, num_pages: u64) -> (Page<S>, Page<S>) {
+        assert!(num_pages > 0);
+        assert!(num_pages <= self.available());
+
+        let start = self.next;
+        self.next += num_pages;
+        let end = self.next - 1;
+
+        (start, end)
+    }
+
+    fn dealloc(&mut self, start: Page<S>, end: Page<S>) {
+        // do nothing
+    }
+}
+
